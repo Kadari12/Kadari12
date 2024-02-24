c113d081e4b550e5cd23fdc0713042526c1fc4e8
Kadari12 <143111149+Kadari12@users.noreply.github.com>
2024-02-24 21:21:10 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,17 @@
+class odd
+{
+public static void main(String[] args)
+{
+int n=45324;
+int getlastdigit=0;
+while(n>0)
+{
+getlastdigit=n%10;
+if(getlastdigit%2!=0)
+{
+System.out.println(getlastdigit);
+}
+n=n/10;
+}
+}
+}
