2adc7cc5508c88da9623aff70d93ca5a7ee66ea4
Kadari12 <143111149+Kadari12@users.noreply.github.com>
2024-02-24 21:26:12 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,18 @@
+class sum
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=1000;i++)
+{
+int total=0;
+for(int j=1;j<=i/2;j++)
+{
+if(j*j==i)
+{
+}
+total=total+i;
+}
+System.out.println(total);
+}
+}
+}
