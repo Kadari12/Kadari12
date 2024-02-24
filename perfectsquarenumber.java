8ad64c5008eb46688c07d6ba17306fee7cf401e7
Kadari12 <143111149+Kadari12@users.noreply.github.com>
2024-02-24 21:24:48 +0530

    Create perfectsquarenumber.java

---
+++ perfectsquarenumber.java
@@ -0,0 +1,16 @@
+class perfectsquarenumber
+{
+public static void main(String[] args)
+{
+for(int a=1;a<=10;a++)
+{
+for(int i=1;i<=a/2;i++)
+{
+if(i*i==a)
+{
+}
+}
+System.out.println("i");
+}
+}	
+}
