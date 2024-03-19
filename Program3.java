4e80516111ce5b676cdd7283e605a0bd5537e56b
Kadari12 <143111149+Kadari12@users.noreply.github.com>
2024-03-19 10:41:34 +0530

    Create Program3.java

---
+++ Program3.java
@@ -0,0 +1,18 @@
+class Program3 
+{
+	public static void main(String[] args) 
+	{
+		Program3 res=new Program3();
+		res.operation();
+	}
+	public void operation()	
+	{
+		int a=15;
+		int b=8;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
